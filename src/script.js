363dc6f2dfdae5bca7c1e56ab2c4724bfd8e28b9
sirdaryolizing <56346276+sirdaryolizing@users.noreply.github.com>
2019-10-12 13:38:17 +0500

    slick slider and our partners

--- src/script.js
+++ src/script.js
@@ -1,33 +1,39 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlide(){
-    showSlides(slideIndex += 1);
-}
-
-function minusSlide(){
-    showSlides(slideIndex -= 1);
-}
-
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n){
-    var i,
-        slides = document.getElementsByClassName("items");
-
-    if(n > slides.length){
-        slideIndex = 1;
-    }
-    
-    if(n < 1){
-        slideIndex = slides.length;
-    }
-
-    for(i = 0; i < slides.length; i++){
-        slides[i].style.display = 'none';
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-}
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 300,
+        dots: false,
+        arrows: false,
+        fade: true,
+    });
+    $('.partners').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 5,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '20px',
+            slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '20px',
+            slidesToShow: 1
+            }
+        }
+        ]
+    });
+});
